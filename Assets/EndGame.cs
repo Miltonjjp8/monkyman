fea571e868f006be5b2dd8425f83bae0ba804aae
Aldahir <91292306+AldahirG@users.noreply.github.com>
2024-12-06 00:07:28 -0600

    [UPDATE]
    Timer, Clock, Check, BG, Sound Effect & Skybox updated

---
+++ Assets/EndGame.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    AudioSource fuenteAudio;
+    void Start()
+    {
+        fuenteAudio = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        fuenteAudio.Play();
+    }
+     void OnTriggerExit(Collider other)
+    {
+        fuenteAudio.Stop();
+    }
+}
